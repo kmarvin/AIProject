6cbb30ec300062bc6af4b65720d2b2bf31a819eb
Marvin Klaus <marvin.klaus@outlook.de>
2018-02-23 18:37:24 +0100

    reformat code

--- final_folder/code/ui/app/static/view.js
+++ final_folder/code/ui/app/static/view.js
@@ -2,49 +2,55 @@
 
 angular.module('myApp.view', ['ngRoute'])
 
-.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: '/static/view.html',
-        controller: 'ViewCtrl'
-    });
-}])
-
-.controller('ViewCtrl', function ($scope, $http) {
-    $scope.suggestions = [];
-    $scope.input = {text: ""};
-    $scope.settings = {numberSuggestions: 1};
-    $scope.error = false;
-
-    $scope.updateInputText = function() {
-        var text = $scope.input.text;
-
-        $http.post('/computeInput', {text: text, settings: $scope.settings}).then(function successCallback(response) {
-            $scope.error = false;
-            var data = response.data;
-            var uniqueData = data.filter(function(item, pos) {
-				return data.indexOf(item) === pos;
-			});
-            if(uniqueData.length < parseInt($scope.settings.numberSuggestions)) {
-				$scope.error = {message: "Through high probability there are less suggestions than requested", status: "warning"};
-			}
-			$scope.suggestions = uniqueData.map(function(elem) {
-				var placeholder = elem;
-				if(elem === " ") {
-					placeholder = "_";
-				}
-				return {text: elem, placeholder: placeholder};
-			});
-        }, function errorCallback(response) {
-            $scope.error = {message: "Sorry, we got a Server Error", status: "error"};
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: '/static/view.html',
+            controller: 'ViewCtrl'
         });
-    }
+    }])
 
-    $scope.selectText = function(text) {
-        $scope.input.text += text;
-        if(text !== " ") {
-			$scope.input.text += " ";
-		}
+    .controller('ViewCtrl', function ($scope, $http) {
         $scope.suggestions = [];
-        $("#userInput").focus();
-    }
-});
+        $scope.input = {text: ""};
+        $scope.settings = {numberSuggestions: 1};
+        $scope.error = false;
+
+        $scope.updateInputText = function () {
+            var text = $scope.input.text;
+
+            $http.post('/computeInput', {
+                text: text,
+                settings: $scope.settings
+            }).then(function successCallback(response) {
+                $scope.error = false;
+                var data = response.data;
+                var uniqueData = data.filter(function (item, pos) {
+                    return data.indexOf(item) === pos;
+                });
+                if (uniqueData.length < parseInt($scope.settings.numberSuggestions)) {
+                    $scope.error = {
+                        message: "Through high probability there are less suggestions than requested",
+                        status: "warning"
+                    };
+                }
+                $scope.suggestions = uniqueData.map(function (elem) {
+                    var placeholder = elem;
+                    if (elem === " ") {
+                        placeholder = "_";
+                    }
+                    return {text: elem, placeholder: placeholder};
+                });
+            }, function errorCallback() {
+                $scope.error = {message: "Sorry, we got a Server Error", status: "error"};
+            });
+        };
+
+        $scope.selectText = function (text) {
+            $scope.input.text += text;
+            if (text !== " ") {
+                $scope.input.text += " ";
+            }
+            $scope.suggestions = [];
+            $("#userInput").focus();
+        }
+    });
